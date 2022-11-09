63ebc7f647eb0b77fbaf2ea22cef56142aec05d2
Rafael Rios <rvrios22@gmail.com>
2022-11-08 16:20:47 -0800

    add: search bar for cities

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7311f43d0dmshb2521443f957a72p14ce1djsnc76e4c1ffabe',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
